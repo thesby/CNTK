501705f3de1a75f94cb05ef6d1fb9e8a8c8bc736
Gaizka Navarro <gaizna@microsoft.com>
2016-03-15 10:14:57 +0100

    Enables ReaderTests to run in either CPU or GPU configurations

--- Tests/UnitTests/ReaderTests/Common/ReaderTestHelper.h
+++ Tests/UnitTests/ReaderTests/Common/ReaderTestHelper.h
@@ -233,14 +233,14 @@ struct ReaderFixture
 
         for (auto i = 0; i < numFeatureFiles; i++)
         {
-            features.push_back(make_shared<Matrix<ElemType>>(0));
+            features.push_back(make_shared<Matrix<ElemType>>(DEVICEID_AUTO));
             wstring name = numFeatureFiles > 1 ? L"features" + std::to_wstring(i + 1) : L"features";
             map.insert(make_pair(name, features[i]));
         }
 
         for (auto i = 0; i < numLabelFiles; i++)
         {
-            labels.push_back(make_shared<Matrix<ElemType>>(0));
+            labels.push_back(make_shared<Matrix<ElemType>>(DEVICEID_AUTO));
             wstring name = numLabelFiles > 1 ? L"labels" + std::to_wstring(i + 1) : L"labels";
             map.insert(make_pair(name, labels[i]));
         }
